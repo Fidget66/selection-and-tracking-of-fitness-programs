5ad26f90a99f1d46f812ac13583af59edf91d743
Роман Макуль <Busnab@mail.ru>
2021-10-09 02:41:43 +0300

    add my app

---
+++ src/main/java/com/makul/fitness/myfitnessprogram/MyFitnessProgramApplication.java
@@ -0,0 +1,13 @@
+package com.makul.fitness.myfitnessprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFitnessProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFitnessProgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/makul/fitness/myfitnessprogram/MyFitnessProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makul.fitness.myfitnessprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFitnessProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
