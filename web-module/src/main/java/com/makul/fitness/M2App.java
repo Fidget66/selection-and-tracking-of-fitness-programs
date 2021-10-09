052096cbbca630290cf16d6d39102e652256c0c5
Роман Макуль <Busnab@mail.ru>
2021-10-09 04:34:45 +0300

    add modulesp

---
+++ web-module/src/main/java/com/makul/fitness/M2App.java
@@ -0,0 +1,11 @@
+package com.makul.fitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2App {
+    public static void main(String[] args) {
+        SpringApplication.run(M2App.class, args);
+    }
+}
